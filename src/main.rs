b9e9f5e1ba8b6d6c66757976b4dd64ad1e3879ea
nnenn0 <53108997+nnenn0@users.noreply.github.com>
2021-05-22 15:19:33 +0900

    add 説明文

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod data_structure;
+
 fn main() {
     println!("Hello, algo-lib-rust!");
+    println!("please type \"cargo test {{ test function }}\"");
+    println!("example: cargo test union_find_test");
 }
