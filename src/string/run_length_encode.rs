a74015debdd9ec780713438314275954e5ccd89a
nnenn0 <nnenn0memo@gmail.com>
2021-05-30 13:29:04 +0900

    update 関数名

--- src/string/run_length_encode.rs
+++ src/string/run_length_encode.rs
@@ -3,7 +3,7 @@
     文字列の可逆圧縮の一種。
 */
 
-pub fn rle_encode(s: Vec<char>) -> Vec<(char, usize)> {
+pub fn run_length_encode(s: Vec<char>) -> Vec<(char, usize)> {
     let n = s.len();
     let mut ret = Vec::new();
     let mut l = 0;
@@ -18,7 +18,7 @@ pub fn rle_encode(s: Vec<char>) -> Vec<(char, usize)> {
     ret
 }
 
-fn rle_decode(code: Vec<(char, usize)>) -> String {
+fn run_length_decode(code: Vec<(char, usize)>) -> String {
     let mut ret = String::new();
     for (c, c_size) in code {
         for _ in 0..c_size {
@@ -29,15 +29,15 @@ fn rle_decode(code: Vec<(char, usize)>) -> String {
 }
 
 #[test] 
-fn run_length_encoding_test() {
+fn run_length_test() {
     let s = "aabbbaad".chars().collect::<Vec<char>>();
-    let encode = rle_encode(s);
+    let encode = run_length_encode(s);
     let mut res = String::new();
     for (c, c_size) in encode.iter() {
         res.push(*c);
         res.push(std::char::from_digit(*c_size as u32, 10).unwrap());
     }
     assert_eq!("a2b3a2d1", res);
-    let res = rle_decode(encode);
+    let res = run_length_decode(encode);
     assert_eq!("aabbbaad", res);
 }
